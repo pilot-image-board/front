5af440b7d69ffba9358280edf70a4860f87ffaca
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2022-12-30 19:00:55 +0100

    feat(security): add check for jwt authenticity

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -2,23 +2,49 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { User } from "@/models";
 import axios from "axios";
-import { API_BASE_URL, refreshApiService } from "@/services";
+import { API_BASE_URL, authService, refreshApiService } from "@/services";
+import { KEYUTIL, KJUR, RSAKey } from "jsrsasign";
+
+const publicKey = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlGElwh6Dn/rsPMBXBo3u
+0uOZfWCqlk7mlM+/Azd625aTe7bNsXxoKrb4GNm+D1Cg1WQdhODI/fv9avSyyZ95
+ps/A/amvEY39+BSX0nV1xak9NPSOqcEdPlqG9ya/GOcf+ZmbqlrOgeQovu9CZ8S6
+zirwWkYCiUFnajr+NW8joo8mjiIgNSho0A+18Ae/JTBM2tK7vif9FrqfhVl7yl0d
+3Bs6MbR3tTrnOeI+vQprEplyR16yeEGBsPhSBFAsZPN7KRCVJkpnmRC+eH3jUevm
+v6n5JOXk6zE1dAmJrUpIS/opWnKo1usqhRyDtt6tIyjlmSjXIHLuuLqNDdt4HUFN
+LwIDAQAB
+-----END PUBLIC KEY-----
+`;
 
 export const useUserStore = defineStore("user", () => {
   const user = ref({} as User);
 
-  const initUser = () => {
+  const initUser = async () => {
     const token = localStorage.getItem("access_token");
     if (token) {
-      const base64Url = token.split(".")[1];
-      const base64 = base64Url.replace("-", "+").replace("_", "/");
-      const decodedToken = JSON.parse(window.atob(base64));
-      user.value = {
-        id: decodedToken.id,
-        username: decodedToken.username,
-        isAdmin: decodedToken.isAdmin,
-        roles: decodedToken.roles,
-      };
+      const key = KEYUTIL.getKey(publicKey);
+      if (!(key instanceof RSAKey)) {
+        console.log("Invalid public key");
+        return;
+      }
+      const isValid = KJUR.jws.JWS.verifyJWT(token, key, {
+        alg: ["RS256"],
+      });
+      if (!isValid) {
+        try {
+          if (await authService.refresh()) {
+            initUser();
+          } else {
+            emptyUser();
+            return;
+          }
+        } catch (error) {
+          emptyUser();
+          return;
+        }
+      }
+      const payload = KJUR.jws.JWS.parse(token).payloadObj;
+      user.value = payload as User;
     }
   };
 
